4229958ddc2122a04a13e5f05be303db4ea91fa6
rebykimani <beckireby15@gmail.com>
2022-11-30 00:40:11 +0300

    completed assignment

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'plant/name'
+  get 'plant/image'
+  get 'plant/price:decimal'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
